2312c7015f9df7a88eef49148b7f74206456d586
Frank Pommerening <frank@pommerening-online.de>
2017-10-17 19:37:53 +0200

    Add sample faceapi

---
+++ FaceApi/DetectFaces.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FP.Study.KNN.FaceApi
+{
+    public class DetectFaces
+    {
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        private readonly string _imageFilePath;
+
+        private readonly string _key;
+
+        public DetectFaces(string imageFilePath, string subscriptionKey)
+        {
+            _imageFilePath = imageFilePath;
+            _key = subscriptionKey;
+        }
+
+        public async Task Run()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _key);
+                var requestParameters = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,smile,glasses,emotion";
+                var uri = $"{uriBase}?{requestParameters}";
+
+                var byteData = GetImageAsByteArray(_imageFilePath);
+
+                using (var content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    var response = await client.PostAsync(uri, content);
+                    Result = await response.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        public string Result { get; private set; }
+
+        public string ResultFormated
+        {
+            get
+            {
+                dynamic parsedJson = JsonConvert.DeserializeObject(Result);
+                return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+            }
+        }
+
+        private byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            using (var fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            using (var binaryReader = new BinaryReader(fileStream))
+            {
+                return binaryReader.ReadBytes((int) fileStream.Length);
+            }
+        }
+    }
+}
---
+++ FaceApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FP.Study.KNN.FaceApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bitte geben Sie den Face-API Key ein:");
+                var key = Console.ReadLine();
+                Console.WriteLine("Bitte geben Sie den Pfad zum Analysebild ein oder leer für Beispielbild.");
+                var path = Console.ReadLine();
+                if (string.IsNullOrEmpty(path))
+                {
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "sample.jpeg");
+                }
+                var detectFaces = new DetectFaces(path, key);
+                detectFaces.Run().Wait();
+                Console.WriteLine($"Analyseergebnis für Datei {Path.GetFileName(path)}");
+                Console.WriteLine(detectFaces.ResultFormated);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fehler bei der Ausführung der Analyse");
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
