098315651f9390f21f07d09146772a6e865d42e0
Frank Pommerening <frank.pommerening@axp-consulting.de>
2017-10-20 18:08:28 +0200

    Add sample for BackPropagation

--- BackPropagation/Program.cs
+++ BackPropagation/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DotNeuralNet;
+using DotNeuralNet.BackPropagation;
 
 namespace FP.Study.KNN.BackPropagation
 {
@@ -6,7 +8,34 @@ namespace FP.Study.KNN.BackPropagation
     {
         static void Main(string[] args)
         {
-            
+            var network = new Network(2, 4, 1);
+
+
+            //Helper_InitWeights(network);
+
+            var trainer = new BackPropagationTrainer(network);
+
+            var rows = new[] {
+                new BackPropagationTrainingRow(new[] { -1.0, -1.0}, new[] { -1.0  }) ,
+                new BackPropagationTrainingRow(new[] { -1.0, 1.0}, new[] {-1.0  }) ,
+                new BackPropagationTrainingRow(new[] { 1.0, -1.0}, new[] {-1.0  }) ,
+                new BackPropagationTrainingRow(new[] { 1.0, 1.0}, new[] {1.0  })
+
+                };
+
+            trainer.Train(rows, 0.5, 10000);
+
+           
+            for (var r = 0; r < rows.Length; r++)
+            {
+                network.Invalidate();
+                for (var i = 0; i < rows[r].Inputs.Length; i++)
+                {
+                    network.InputNodes[i].Value = rows[r].Inputs[i];
+                }
+                Console.WriteLine(network.OutputNodes[0].Value);
+            }
         }
+
     }
 }
